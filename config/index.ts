6c1ee810f577c3e091b030d1f8726a44aa69ef23
unknown <prepaddoo@gmail.com>
2025-02-03 17:31:49 +0100

    First Commit

--- config/index.ts
+++ config/index.ts
@@ -1,18 +1,26 @@
 import type { Metadata } from 'next'
 
 export const siteConfig: Metadata = {
+
+title: 'SPAZIO UNICO: Trasforma la tua stanza con l&apos;intelligenza artificialei',
+description: 'Carica le foto della tua stanza e scegli la modalità e lo stile che desideri. L&apos;intelligenza artificiale genererà idee di design personalizzate in base ai tuoi input.',
+keywords: ['progettazione degli interni','progettazione dello spazio', 'progettazione degli interni', 'progettista della stanza','generatore di stanze','idee per il soggiorno', 'generatore di mobili', 'progettista di mobili', 'spazi interni',],
+openGraph: {
   title: 'SPAZIO UNICO: Trasforma la tua stanza con l&apos;intelligenza artificialei',
   description: 'Carica le foto della tua stanza e scegli la modalità e lo stile che desideri. L&apos;intelligenza artificiale genererà idee di design personalizzate in base ai tuoi input.',
-  keywords: [
-    'progettazione degli interni',
-    'progettazione dello spazio',
-    'progettazione degli interni',
-    'progettista della stanza',
-    'generatore di stanze',
-    'idee per il soggiorno',
-    'generatore di mobili',
-    'progettista di mobili',
-    'spazi interni',
-  ] ,
- 
-}
+  url: 'https://www.spaziounico.com',
+  siteName: 'Spazio Unico',
+  images: [
+    {
+      url: 'https://www.spaziounico.com/_next/image?url=%2Flogo1.png&w=1920&q=75',
+      width: 1200,
+      height: 630,
+      alt: 'Spazio Unico',
+    },
+  ],
+  locale: 'en_US',
+  type: 'website',
+},
+
+
+};
